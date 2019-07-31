5e3bc8a48b1c1f3617af1a39e26921dc3fda2bb2
Stephen Blythe <stephenblythe@gmail.com>
2019-07-31 20:00:24 +0100

    Adds find album by ID method

--- models/Album.rb
+++ models/Album.rb
@@ -42,6 +42,19 @@ class Album
     sql = "UPDATE albums SET (title, genre, artist_id) = ($1, $2, $3) WHERE id = $4"
     values = [@title, @genre, @artist_id, @id]
     SqlRunner.run(sql, values)
-  end 
+  end
+
+  def delete()
+    sql = "DELETE FROM albums WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM albums where id = $1"
+    values = [id]
+    artist = SqlRunner.run(sql, values)[0]
+    return Album.new(artist)
+  end
 
 end
